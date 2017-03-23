ca2004da69ba97c15c60b806fb20091b035d0554
musgravejw <musgravejw@gmail.com>
2017-03-23 16:26:20 -0400

    scale up deployment replicas

--- hello/start.sh
+++ hello/start.sh
@@ -1,6 +1,6 @@
 docker build -t hello:v2 .
 
-kubectl run hello --image=hello:v1 --port=8080
+kubectl run hello --image=hello:v2 --replicas=3 --port=8080
 kubectl expose deployment hello --type=LoadBalancer
 kubectl set image deployment/hello hello=hello:v2
 
